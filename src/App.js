1bb1a812c3511450063735f6e60bb43b134aa40b
Ediberto Ponce <ediberto01@gmail.com>
2021-09-20 20:01:46 -0700

    Finished Form Layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import { Component } from 'react';
 import axios from 'axios';
+import Header from './Componenents/Header.js'
+import Footer from './Componenents/Footer.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container'
 import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 class App extends Component {
   constructor(props) {
@@ -35,41 +42,60 @@ class App extends Component {
 
   render() {
     return (
+
+
       <>
-        <input onChange={(event) => this.setState({ searchQuery: event.target.value })} placeholder="Search for a city!"></input>
-        <button onClick={this.getLocation}>Explore!</button>
+        <Header />
+        <Container fluid>
+          <Form>
+            <Row className="justify-content-md-center">
+              <Col sm={3} className="my-1">
+                <Form.Label htmlFor="inlineFormInputName" visuallyHidden>
+                  Please Enter City Name
+                </Form.Label>
+                <Form.Control onChange={(event) => this.setState({ searchQuery: event.target.value })} placeholder="Ex: Seattle" />
+              </Col>
+              <Col xs="auto" className="my-1">
+                <Button onClick={this.getLocation} as="input" type="submit" value="Submit" variant="primary" />{' '}
+                {/* <Button onClick={this.getLocation} variant="primary">Explore!</Button>{' '} */}
+              </Col>
+            </Row>
+          </Form>
 
-{/* 
-        <Form>
-          <Row className="align-items-center">
-            <Col sm={3} className="my-1">
-              <Form.Label htmlFor="inlineFormInputName" visuallyHidden>
-                Please Enter City Name
-              </Form.Label>
-              <Form.Control id="inlineFormInputName" placeholder="Jane Doe" />
+
+          <Row className="justify-content-md-center">
+            <Col>
+              {this.state.location.place_id &&
+                <h3>The city is: {this.state.location.display_name}</h3>
+              }
             </Col>
-            <Col xs="auto" className="my-1">
-              <Button type="submit">Submit</Button>
+            <Col>
+              {this.state.location.place_id &&
+                <h3> Latitude: {this.state.location.lat}  |  Longitude: {this.state.location.lon}</h3>
+              }
             </Col>
+
+            {
+              this.state.error && <h3>Please enter a city (make sure you're spelling it correctly)</h3>
+            }
+
           </Row>
-        </Form> */}
 
 
 
+        </Container>
+
+        <Footer />
 
 
-        {this.state.location.place_id &&
-          <h2>The city is: {this.state.location.display_name}</h2>
-        }
-        {this.state.location.place_id &&
-          <h2> Latitude: {this.state.location.lat}  |  Longitude: {this.state.location.lon}</h2>
-        }
-
-        {
-          this.state.error && <h2>Please enter a city (make sure you're spelling it correctly)</h2>
-        }
 
       </>
+
+
+
+
+
+
     )
   }
 }
---
+++ src/Componenents/Footer.js
@@ -0,0 +1,23 @@
+import { Component } from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+
+
+
+class Footer extends Component {
+  render() {
+    return (
+      <>
+        <Navbar bg="dark" variant="dark">
+          <Container className="justify-content-md-center">
+            <Navbar.Brand href="#home">
+              Back to the Top
+            </Navbar.Brand>
+          </Container>
+        </Navbar>
+      </>
+    )
+  }
+}
+
+export default Footer;
